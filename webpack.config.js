8c2f36201441becb1eb0ba051324638f79068a64
NikolajTaskin <nikolajtaskin@yahoo.com>
2020-09-22 09:56:04 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+let path = require('path');
+
+module.exports = {
+	entry: __dirname + "/src/app.js",
+	output: {
+		path: __dirname + "/public/",
+		filename: 'app.js'
+	
+},
+
+	devServer: {
+		contentBase: path.join(__dirname, '/public'),
+		compress: true,
+		port: 9002
+	},
+	watch: true,
+	watchOptions: {
+		ignored:/node_modules/
+	},
+	module: {
+	  rules: [
+	    {
+	      test: /\.m?js$/,
+	      exclude: /(node_modules|bower_components)/,
+	      use: {
+	        loader: 'babel-loader',
+	        options: {
+	          presets: ['@babel/preset-env'],
+	          plugins: ['@babel/plugin-proposal-object-rest-spread']
+	        }
+	      }
+	    }
+	  ]
+	}
+}
